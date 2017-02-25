f507d474b0b2dac40b7b3797d1240bb3f9d582bd
wieckowski.bw <bartlomiej.wieckowski@lingaro.com>
2017-02-25 14:57:01 +0100

    Create and Get method

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {StockService} from '../stock.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  stocks : string[];
+
 
   ngOnInit() {
+    this.getAllStocks();
   }
 
+  constructor(private stockService: StockService) {
+
+  }
+
+  getAllStocks(){
+    this.stockService.getStocksAPI()
+      .subscribe(
+        data => this.stocks = data,
+        error => console.log('Server Error2')
+      );
+  }
+
+  createStock(newStockCode : string, newName : string) {
+    this.stockService.createStock(newStockCode, newName).subscribe();
+    location.reload();
+  }
 }
